ea98e6df7fd1e1d7a5fec4888d80e2d8f5e79823
Yakir Abraham Reshef <yar2@loge.orchestra>
2016-09-20 23:00:48 -0400

    vis bugfix

--- vis.py
+++ vis.py
@@ -1,16 +1,11 @@
 from __future__ import print_function, division
 import numpy as np
 import pandas as pd
+from matplotlib import gridspec
 import matplotlib.pyplot as plt
 
-def bin_on_x(x, y, nbins = 25):
-    df = pd.dataframe({'x':x, 'y':y})
-    df['group_index'] = map(int, (nbins-0.001)*(x-min(x))/(max(x)-min(x)))
-    for g in range(nbins):
-        pass
-    #TODO
 
-def qqplot(pvals, minuslog10p=False, fname=None, show_anyway=False, text=''):
+def qqplot(pvals, minuslog10p=False, text=''):
     x = np.arange(1/len(pvals), 1+1/len(pvals), 1/len(pvals))[:len(pvals)]
     logx = -np.log10(x)
     maxy = 3*np.max(logx)
@@ -27,11 +22,38 @@ def qqplot(pvals, minuslog10p=False, fname=None, show_anyway=False, text=''):
     plt.ylabel('-log10(p)')
     plt.title(text)
 
-    if fname:
-        plt.savefig(fname)
-        if show_anyway:
-            plt.show()
-        plt.clf()
-    else:
-        plt.show()
+# creates a scatter plot where x is binned and y is averaged within each bin
+def scatter_b(x, y, nbins=25, **kwargs):
+    boundaries = np.linspace(min(x), max(x), nbins)
+    bins =  zip(boundaries[:-1], boundaries[1:])
 
+    binx, biny = np.empty(len(bins)), np.empty(len(bins))
+    binx[:] = np.nan; biny[:] = np.nan
+    for i, (l, r) in enumerate(bins):
+        binx[i] = np.mean(x[(x>=l)&(x<r)])
+        biny[i] = np.mean(y[(x>=l)&(x<r)])
+
+    plt.scatter(binx, biny, **kwargs)
+
+# creates a scatter plot where x is smoothed over some window
+def scatter_s(x, y, windowsize=100, **kwargs):
+    df = pd.DataFrame({'x':x, 'y':y})
+    df.sort(columns='x', inplace=True)
+    #TODO
+
+# creates a scatter plot with marginal distribution histograms. kwargs are passed to the
+# scatter function
+def scatter_m(x, y, xbins=None, ybins=None, text='', **kwargs):
+    if not xbins:
+        xbins = int(min(len(x)/20, 100))
+    if not ybins:
+        ybins = int(min(len(y)/20, 100))
+
+    gs = gridspec.GridSpec(2,2, width_ratios=[3,1], height_ratios=[1,3])
+    ax10 = plt.subplot(gs[1,0])
+    ax10.scatter(x,y, **kwargs); ax10.set_title(text)
+    ax00 = plt.subplot(gs[0,0])
+    ax00.hist(x, bins=xbins, normed=True)
+    ax11 = plt.subplot(gs[1,1])
+    ax11.hist(y, bins=ybins, orientation='horizontal', normed=True)
+    plt.title(text)
