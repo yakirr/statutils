112eb3c6708367bb4136786ecc6bbcf56ede6366
Yakir Abraham Reshef <yar2@loge.orchestra>
2016-11-07 08:40:13 -0500

    added storey-tibshirani FDR control

--- sig.py
+++ sig.py
@@ -2,17 +2,48 @@ from __future__ import print_function, division
 import numpy as np
 from scipy import stats
 
-def fdr(pvals, minuslog10p=False, threshold=0.05):
+
+def st_fdr(pvals, threshold=0.05):
+    qvals, pi0 = info(pvals)
+    numsig = (qvals <= threshold).sum()
+    return np.nan, numsig, pi0
+
+def info(pvals):
+    from rpy2.robjects import r, pandas2ri
+    pandas2ri.activate()
+    from rpy2.robjects.packages import importr
+    q = importr('qvalue')
+    def py(x):
+        return pandas2ri.ri2py(x)
+
+    qobj = q.qvalue(pvals)
+    pi0 = py(qobj.rx2('pi0'))[0]
+    qvals = py(qobj.rx2('qvalues'))
+    return qvals, pi0
+
+def sigrows(df, pvals, threshold=0.05, qname='q'):
+    qvals, _ = info(pvals)
+    sigdf = df[qvals <= threshold]
+    sortedindices = [x for (y,x) in sorted(zip(qvals[qvals<=threshold],sigdf.index.values))]
+    return sigdf.loc[sortedindices]
+
+
+# st = use Storey-Tibshirani R package
+def fdr(pvals, minuslog10p=False, threshold=0.05, st=True):
     if minuslog10p:
         pvals = np.power(10, -pvals)
     pvals = pvals[~np.isnan(pvals)] # filter out nans so we don't count them as tests
-    pvals = np.sort(pvals)
-    cs = np.arange(1,len(pvals)+1)*threshold/len(pvals)
-    cutoff_p = 0
-    for c,p in zip(cs, pvals):
-        if p < c:
-            cutoff_p = p
-    return cutoff_p, np.sum(pvals <= cutoff_p)
+
+    if st:
+        return st_fdr(pvals, threshold=threshold)
+    else:
+        pvals = np.sort(pvals)
+        cs = np.arange(1,len(pvals)+1)*threshold/len(pvals)
+        cutoff_p = 0
+        for c,p in zip(cs, pvals):
+            if p < c:
+                cutoff_p = p
+        return cutoff_p, np.sum(pvals <= cutoff_p), np.nan
 
 
 if __name__ == '__main__':
